68f00da015150c660d12e80b80a4005d8957311a
Vinicius R Ferreira <viniguatu@gmail.com>
2026-03-24 18:28:49 -0300

    Subindo os arquivos do exercicio 5

---
+++ src/main/java/com/example/aula2/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.example.aula2.repository;
+
+import com.example.aula2.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
